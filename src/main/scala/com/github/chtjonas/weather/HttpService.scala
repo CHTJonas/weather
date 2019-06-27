d82c72c029e33582cd0fab89e90e4debc723d3e4
Charlie Jonas <charlie@charliejonas.co.uk>
2019-06-27 23:48:33 +0100

    Proxy parsed RSS to frontend

--- src/main/scala/com/github/chtjonas/weather/HttpService.scala
+++ src/main/scala/com/github/chtjonas/weather/HttpService.scala
@@ -2,11 +2,19 @@ package com.github.chtjonas.weather
 
 import com.twitter.finagle.Service
 import com.twitter.finagle.http
-import com.twitter.util.Future
+import com.twitter.util.{Await,Future}
 
 object HttpService {
   val service = new Service[http.Request, http.Response] {
-    def apply(req: http.Request): Future[http.Response] =
-      RssClient.response
+    def apply(req: http.Request): Future[http.Response] = {
+      val response = http.Response(req.version, http.Status.Ok)
+      Await.ready(RssClient.response).onSuccess { rssResponse: http.Response =>
+        val xmlString = rssResponse.contentString
+        val map = RssParser.parse(xmlString)
+        val responseBody = map.toString()
+        response.setContentString(responseBody)
+      }
+      Future.value(response)
+    }
   }
 }
