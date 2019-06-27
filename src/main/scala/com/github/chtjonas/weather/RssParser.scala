b6ff0eb5200a88460ad94990b7c69e32b799bc6d
Charlie Jonas <charlie@charliejonas.co.uk>
2019-06-27 20:02:31 +0100

    Finish parser for weather station RSS

--- src/main/scala/com/github/chtjonas/weather/RssParser.scala
+++ src/main/scala/com/github/chtjonas/weather/RssParser.scala
@@ -3,20 +3,31 @@ package com.github.chtjonas.weather
 import scala.xml.{Elem, XML}
 
 object RssParser {
-  def parse(xml: String): Elem =
-    XML.loadString(xml)
+  def toXml(str: String): Elem =
+    XML.loadString(str)
 
-  def retrieveData(xml: Elem): Map[String, String] = {
+  def parse(responseText: String): Map[String, String] = {
+    val str = responseText.replace("<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>", "")
+    val xml = toXml(str)
+    parseData(xml)
+  }
+
+  def parseData(xml: Elem): Map[String, String] = {
     var map: Map[String, String] = Map()
     val descs = xml \\ "description"
     val table = descs(1).text
-    val rows = parse(table) \\ "tr"
+    val rows = toXml(table) \\ "tr"
     rows.foreach(row => {
       val columns = row \\ "td"
       val key = columns(0).text
-      val value = columns(1).text
+      val value = removeUnits(columns(1).text)
       map += (key -> value)
     })
     return map
+  }
+
+  def removeUnits(value: String): String = {
+    val units = List("deg", "mm/1000", "knots", "%", "C", "mBar", "secs")
+    value.split(" ").map { word => if (units.contains(word)) "" else word }.mkString
   }
 }
